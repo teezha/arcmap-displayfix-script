a847e2c192eb2daa4f99813675c3161bba3bb865
teezha <genggengz@gmail.com>
2017-02-14 17:56:02 -0800

    v2

--- fix-mxd-paths.py
+++ fix-mxd-paths.py
@@ -5,12 +5,16 @@ oldDataPath = arcpy.GetParameterAsText(0)
 newGeoDB = arcpy.GetParameterAsText(1)
 
 mxd = arcpy.mapping.MapDocument("CURRENT")
-mxd.findAndReplaceWorkspacePaths(oldDataPath, newGeoDB, False)
+mxd.findAndReplaceWorkspacePaths(oldDataPath, newGeoDB)
 
 arcpy.RefreshTOC()
+arcpy.RefreshActiveView()
 
 elementsArray = ["DATAFRAME_ELEMENT", "GRAPHIC_ELEMENT", "MAPSURROUND_ELEMENT", "PICTURE_ELEMENT", "TEXT_ELEMENT"]
 
 for index in range(len(elementsArray)):
     arcpy.mapping.ListLayoutElements(mxd,elementsArray[index])
-    arcpy.AddMessage("Map Element: "+elementsArray[index])
+    msg= arcpy.mapping.ListLayoutElements(mxd,elementsArray[index])
+    arcpy.AddMessage("Map Element: "+msg)
+
+mxd.save()
