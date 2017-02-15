00561b5ee07507cedb79533fb0222af8edc85ab9
teezha <genggengz@gmail.com>
2017-02-14 18:30:42 -0800

    inital commit

---
+++ fix-lyr-paths.py
@@ -0,0 +1,27 @@
+import arcpy
+import os
+
+lyrPath = arcpy.GetParameterAsText(0)
+
+mxd = arcpy.mapping.MapDocument("CURRENT")
+
+homeFrame = mxd.activeDataFrame
+for data in homeFrame:
+    if data.name == "Layers":
+        thisFrame = data
+
+lExt = lyrPath.getExtent()
+fExt = thisFrame.extent
+fExt = lExt
+thisFrame.extent = fExt
+
+arcpy.RefreshActiveView()
+
+elementsArray = ["DATAFRAME_ELEMENT", "GRAPHIC_ELEMENT", "MAPSURROUND_ELEMENT", "PICTURE_ELEMENT", "TEXT_ELEMENT"]
+
+for index in range(len(elementsArray)):
+    arcpy.mapping.ListLayoutElements(mxd,elementsArray[index])
+    msg= arcpy.mapping.ListLayoutElements(mxd,elementsArray[index])
+    arcpy.AddMessage("Map Element: "+msg)
+
+mxd.save()
